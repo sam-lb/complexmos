5cd6bb5fc90828b5658e00057a4efb95a24f544a
sam-lb <samlbrunacini@gmail.com>
2024-05-03 10:39:39 -0400

    implement Trie data structure for prefix search

--- parsing/trie.js
+++ parsing/trie.js
@@ -18,6 +18,10 @@ class Node {
         this.next = next;
     }
 
+    setChild(child) {
+        this.child = child;
+    }
+
     hasNext() {
         return this.next !== null;
     }
@@ -55,27 +59,32 @@ class LinkedList {
             node.setNext(newNode);
         }
         this.size++;
+        return newNode;
     }
 
     remove(value) {
+        /*
+        Remove value from the linked list.
+        Return boolean indicating if successfully removed
+        */
         if (this.head === null) {
-            return -1;
+            return false;
         } else {
             if (this.head.value === value) {
                 this.head = this.head.next;
                 this.size--;
-                return 1;
+                return true;
             } else {
                 let node = this.head;
                 while (node.hasNext()) {
                     if (node.next.value === value) {
                         node.next = node.next.next;
                         this.size--;
-                        return 1;
+                        return true;
                     }
                     node = node.next;
                 }
-                return -1;
+                return false;
             }
         }
     }
@@ -113,24 +122,81 @@ class Trie {
 
     static TERMINATOR = "<end>";
 
-    constructor() {
-        this.root = null();
+    constructor(keysArray=null) {
+        this.root = null;
+
+        if (keysArray !== null) {
+            for (let key of keysArray) {
+                this.insertKey(key);
+            }
+        }
     }
 
-    insertKey() {
+    insertKey(key) {
+        if (this.root === null) {
+            this.root = new LinkedList();
+        }
 
+        let list = this.root;
+        for (let i=0; i<key.length; i++) {
+            const character = key[i];
+            const node = list.search(character);
+            if (node === null) {
+                const newNode = list.push(character);
+                newNode.setChild(new LinkedList());
+                list = newNode.child;
+            } else {
+                list = node.child;
+            }
+
+            if (i === key.length - 1 && list.search(Trie.TERMINATOR) === null) {
+                list.push(Trie.TERMINATOR);
+            }
+        }
     }
 
-    containsKey() {
+    containsKey(key) {
+        if (this.root === null) return false;
 
+        let list = this.root;
+        for (let character of key) {
+            const node = list.search(character);
+            if (node === null) return false;
+            list = node.child;
+        }
+        return list.search(Trie.TERMINATOR) !== null;
     }
 
-    containsPrefix() {
+    containsPrefix(prefix) {
+        if (this.root === null) return false;
 
+        let list = this.root;
+        for (let character of prefix) {
+            const node = list.search(character);
+            if (node === null) return false;
+            list = node.child;
+        }
+        return true;
     }
 
-    remove() {
-
+    remove(key) {
+        /*
+        Remove key from trie, return boolean indicating if the removal was successful
+        Note: does NOT remove prefixes.
+        For example:
+        const trie = new Trie(["dog"]);
+        trie.remove("dog"); // true
+        trie.containsKey("dog"); // false
+        trie.containsPrefix("dog"); // true
+        */
+        if (this.root === null) return false;
+        let list = this.root;
+        for (let character of key) {
+            const node = list.search(character);
+            if (node === null) return false;
+            list = node.child;
+        }
+        return list.remove(Trie.TERMINATOR);
     }
 
 }
