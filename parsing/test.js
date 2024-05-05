8546aa9a61947bdec22f58d4628be4c7a9602114
sam-lb <samlbrunacini@gmail.com>
2024-05-05 17:46:03 -0400

    reorganize core math libraries into a folder

---
+++ parsing/test.js
@@ -0,0 +1,28 @@
+document.querySelector("#minput-field").focus();
+
+const tracker = new ErrorTracker("error-output", "Tokenization successful!");
+
+const scope = {
+    "builtin": [
+        "sin", "sinh", "max",
+    ],
+};
+
+
+function handleKeydown() {
+    if (window.event.keyCode === 13) {
+        handleSubmit();
+    }
+}
+
+
+function handleSubmit() {
+    const latex = document.querySelector("#minput-field").value;
+    const text = cleanLatex(latex);
+    const tokens = tokenize(text, tracker, scope);
+    console.log(tokens);
+
+    if (tokens !== null) {
+        tracker.clear();
+    }
+}
