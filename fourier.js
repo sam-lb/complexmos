1d9e0a17258ba3c41615e858b5498efd0472258c
sam-lb <samlbrunacini@gmail.com>
2024-05-04 22:43:19 -0400

    implement Fourier coefficient calculations and Fourier series

--- fourier.js
+++ fourier.js
@@ -1,3 +1,45 @@
 
 
 
+function parameterizePoints(points) {
+    /*
+    Return a parameteric function consisting of straight lines connecting points
+    Note: does NOT necessarily produce a constant time parameterization
+    */
+
+}
+
+
+
+function fourierCoefficients(f, N) {
+    /*
+    Calculate the fourier coefficients for the complex function f 
+    parameterized on [0, 1]. 
+    Coefficients from -N, -N+1, ... , N-1, N
+    */
+    const coefs = [];
+    for (int n=-N; n<=N; n++) {
+        const F = (t) => {
+            return Complex.mult(f(t), Complex.exp( complex(0, -2 * Math.PI * n) ));
+        };
+        coefs.push(
+            integrateOverParameter(F, 0, 1)
+        );
+    }
+    return coefs;
+}
+
+
+function fourierSeries(f, N) {
+    const coefs = fourierCoefficients(f, N);
+    return (t) => {
+        const result = complex(0, 0);
+        for (let n=-N; n<=N; n++) {
+            const coef = coefs[n + N];
+            result.iadd(
+                Complex.mult(coef, Complex.exp( complex(0, 2 * Math.PI * n) ))
+            );
+        }
+        return result;
+    };
+}
