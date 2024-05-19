5a9d5d08c3505ac47e6830f024e72adb2215a313
sam-lb <samlbrunacini@gmail.com>
2024-05-19 12:48:42 -0400

    update documentation for projection functions

--- math/projection.js
+++ math/projection.js
@@ -1,6 +1,9 @@
 
 
 function stereographic(z) {
+    /**
+     * computes the stereographic projection of the 3d point z onto the xy plane
+     */
     return complex(
         z[0] / (1 - z[2]),
         z[1] / (1 - z[2]),
@@ -8,6 +11,9 @@ function stereographic(z) {
 }
 
 function inverseStereoProject(z) {
+    /**
+     * computes the inverse stereographic projection of a 2d point onto the unit spheree
+     */
     const normSq = z.normSq();
         return matrix([
             (2 * z.re) / (1 + normSq),
@@ -17,6 +23,10 @@ function inverseStereoProject(z) {
 }
 
 function perspectiveProject(Z, orbit, fov) {
+    /**
+     * computes the perspective projection of the 3d point Z onto the xz plane
+     * given the orbit radius and fov values
+     */
     const values = Z.getColumn(0);
     const x = values[0], y = values[1], z = values[2];
     const denom = (orbit + fov * y);
