bea6e27a73fbca6e99e7fda3f49cd786b8a593c4
sam-lb <samlbrunacini@gmail.com>
2024-06-01 15:29:43 -0400

    added callback function to ErrorTracker

--- parsing/errors.js
+++ parsing/errors.js
@@ -3,15 +3,20 @@
 
 class ErrorTracker {
 
-    constructor(errorDivID, successMsg) {
+    constructor(errorDivID, successMsg, callback=null) {
         this.hasError = false;
         this.message = null;
         this.successMsg = successMsg;
         this.target = errorDivID;
+        this.callback = callback;
     }
 
     setTarget(errorDivID) {
         this.target = errorDivID;
+    }
+
+    setCallback(callback) {
+        this.callback = callback;
     }
 
     error(message) {
@@ -22,6 +27,10 @@ class ErrorTracker {
         target.innerHTML = this.message;
         target.style.color = "red";
         target.style.display = "block";
+
+        if (this.callback !== null) {
+            this.callback();
+        }
     }
 
     clear() {
