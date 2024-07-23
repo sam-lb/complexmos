8ac0977078684db5e3196438baef2403b9854188
sam-lb <samlbrunacini@gmail.com>
2024-07-23 18:43:44 -0400

    added a snapshot test

--- tests/complex.test.js
+++ tests/complex.test.js
@@ -1,9 +1,17 @@
 
 
 const { complex, Complex } = require("../math/complex.js");
+const snapShotDiff = require("snapshot-diff");
+
+expect.addSnapshotSerializer(snapShotDiff.getSnapshotDiffSerializer());
 
 
 test("1 + 1 = 2", () => {
     expect(complex(1, 0).add(complex(1, 0)).re).toBe(2);
     expect(complex(1, 0).add(complex(1, 0)).im).toBe(0);
+});
+
+
+test("test complex addition", () => {
+    expect(complex(1.12, 43.5).add(complex(51.309, 12.2))).toMatchSnapshot();
 });
