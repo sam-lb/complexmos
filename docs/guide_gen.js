15954ab81af7f9314548d2e6a606ac7c4cfbf3cb
sam-lb <samlbrunacini@gmail.com>
2024-07-24 18:17:24 -0400

    added descriptions for all functions and variables

--- docs/guide_gen.js
+++ docs/guide_gen.js
@@ -5,7 +5,91 @@ const scopes = require("../app/scope.js");
 const scope = scopes.scope.builtin;
 
 const descriptions = {
-    "Gamma": "The Gamma function \\( \\Gamma(z) \\)",
+    "Gamma": "The Gamma function \\( \\Gamma(z) \\).",
+
+    "acos": "The inverse cosine function.",
+
+    "arg": "The argument or angle of a complex number.",
+
+    "array": "The array datatype. This is currently not yet implemented, but the keyword is already reserved in the calculator.",
+
+    "asin": "The inverse sine function.",
+
+    "atan": "The inverse tangent function.",
+
+    "atanh": "The inverse hyperbolic tangent function.",
+
+    "beta": "The beta function \\( \\beta(z, w) \\).",
+
+    "clamp": "Make sure that \\( |w| \\leq |z| \\leq |t|  \\). If so, return \\( z \\). If not, return \\( z \\) scaled to the norm of the closer bound. For example, \\( \\operatorname{clamp}(1.1, 0, 1)=1 \\).",
+
+    "complex": "The complex datatype. It represents a single number of the form \\( a+ib \\).",
+
+    "conj": "The complex conjugate of a complex number, that is, the reflection of the number across the real axis.",
+
+    "cos": "The cosine function.",
+
+    "cosh": "The hyperbolic cosine function.",
+
+    "e": "Euler's constant, the base of the natural logarithm, \\( e \\approx 2.718 \\).",
+
+    "exp": "The exponential function.",
+
+    "frac": "Returns the fractional part (the part after the decimal point) of both the real and imaginary components of a complex number. For example, \\( \\operatorname{frac}(1.123 + 4.567i)=0.123+0.567i \\).",
+
+    "function": "The function datatype",
+    
+    "i": "The imaginary unit \\( i := x \\in \\mathbb{R}\\left[x\\right]/(x^2+1) \\).",
+
+    "im": "The imaginary part of a complex number.",
+
+    "imagBounds": "The current y-axis bounds in plane mode. The format is \\( \\operatorname{imagBounds} = \\operatorname{yMin} + \\operatorname{yMax}\\cdot i \\).",
+
+    "inv": "The multiplicative inverse of a complex number.",
+
+    "inverseSC": "Construction of the inverse Schwarz-Christoffel map for a regular \\( p \\)-gon. This conformally maps the unit disk to the \\( p \\)-gon.",
+
+    "lerp": "Linearly interpolates \\( z \\) between \\( w \\) and \\( t \\).",
+
+    "ln": "The natural (base \\( e \\) logarithm.",
+
+    "matrix": "The matrix datatype. This is currently not yet implemented, but the keyword is already reserved in the calculator.",
+    
+    "max": "Returns the argument with the greater norm.",
+
+    "min": "Returns the argument with the lesser norm.",
+    
+    "norm": "The Euclidean norm (magnitude) of a complex number.",
+
+    "normSq": "The square of the Euclidean norm (magnitude) of a complex number.",
+
+    "pToPlane": "A conformal map from a regular unit-radius \\( p \\)-gon to the entirety of the complex plane.",
+
+    "pi": "The mathematical half-circle constant \\( \\pi \\approx 3.142 \\).",
+
+    "planeToP": "A conformal map from the complex plane to a regular unit-radius \\( p \\)-gon.",
+
+    "re": "The real part of a complex number.",
+
+    "realBounds": "The current x-axis bounds in plane mode. The format is \\( \\operatorname{imagBounds} = \\operatorname{xMin} + \\operatorname{xMax}\\cdot i \\).",
+
+    "sc": "Construction of the Schwarz-Christoffel map for a regular \\( p \\)-gon. This conformlly maps a regular \\( p \\)-gon to the unit disk.",
+
+    "sin": "The sine function.",
+
+    "sinh": "The hyperbolic sine function.",
+
+    "sqrt": "The square root function.",
+
+    "squeeze": "Smoothly compresses a \\( 2\\cdot\\operatorname{length} \\) by \\( 2\\cdot\\operatorname{length} \\) square centered at the origin to the unit circle. The incircle of the square is mapped to a circle of radius \\( 0 \\leq \\operatorname{coverage} \\leq 1 \\).",
+
+    "tan": "The tangent function.",
+
+    "tanh": "The hyperbolic tangent function.",
+
+    "tau": "The mathematical circle constant \\( \\tau \\approx 6.283 \\).",
+
+    "z": "A parameter representing a complex number that ranges across the entire plane.",
 };
 
 
@@ -27,7 +111,7 @@ const generateDescriptions = () => {
             }
             arguments += "<br>";
         }
-        result += `<div class="description-entry"><span style="font-weight: bold">${key}</span><br>${arguments}Description:${description}</div>`;
+        result += `<div class="description-entry"><span style="font-weight: bold">${key}</span><br>${arguments}Description: ${description}</div>`;
     }
 
     return result;
